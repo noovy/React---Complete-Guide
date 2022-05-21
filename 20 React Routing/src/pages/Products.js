a32decfd99bcb4873fed6c1003253b86ae674fb2
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-21 17:51:39 +0200

    dynamic

--- 20 React Routing/src/pages/Products.js
+++ 20 React Routing/src/pages/Products.js
@@ -1,5 +1,16 @@
 const Products = () => {
-    return <h1>Proucts page</h1>
+  return (
+    <section>
+      <h1>Proucts page</h1>
+      <ul>
+        <li>Product</li>
+        <li>Product</li>
+        <li>Product</li>
+        <li>Product</li>
+        <li>Product</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
