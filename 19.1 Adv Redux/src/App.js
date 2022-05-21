9d9ab51503c29b6136ed38674aec8481b120565f
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-21 16:43:06 +0200

    fetch

--- 19.1 Adv Redux/src/App.js
+++ 19.1 Adv Redux/src/App.js
@@ -1,21 +1,23 @@
-import { Fragment, useEffect } from "react";
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { useSelector, useDispatch } from "react-redux";
-import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
-  const isToggle = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
-  console.log(isToggle);
-  console.log(cart);
-  const notification = useSelector(state => state.ui.notification)
+  const notification = useSelector((state) => state.ui.notification);
 
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if (isInitial) {
@@ -23,15 +25,22 @@ function App() {
       return;
     }
 
-    dispatch(sendCartData(cart));
-
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
     <Fragment>
-      {notification && <Notification status={notification.status} title={notification.title} message={notification.message} /> }
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
       <Layout>
-        {isToggle && <Cart />}
+        {showCart && <Cart />}
         <Products />
       </Layout>
     </Fragment>
