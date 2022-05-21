466921e09a213bbb06215940c0938cebff80b24c
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-21 10:15:35 +0200

    reduxing

--- 19.1 Adv Redux/src/App.js
+++ 19.1 Adv Redux/src/App.js
@@ -1,11 +1,14 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {useSelector} from 'react-redux'
 
 function App() {
+  const isToggle = useSelector((state) => state.ui.cartIsVisible);
+  console.log(isToggle);
   return (
     <Layout>
-      <Cart />
+      {isToggle && <Cart />}
       <Products />
     </Layout>
   );
--- 19.1 Adv Redux/src/components/Cart/CartButton.js
+++ 19.1 Adv Redux/src/components/Cart/CartButton.js
@@ -1,10 +1,21 @@
 import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
+
+  const toggleHandle = () => {
+    dispatch(uiActions.toggle());
+    console.log(toggleHandle);
+  };
+  
+  
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandle}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
