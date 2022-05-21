aaeea0ee3fe2a83a4eddcd99a7d7c771b729de83
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-21 12:39:54 +0200

    firebase

--- 19.1 Adv Redux/src/App.js
+++ 19.1 Adv Redux/src/App.js
@@ -1,11 +1,23 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import {useSelector} from 'react-redux'
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
 
 function App() {
   const isToggle = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
   console.log(isToggle);
+  console.log(cart);
+
+  useEffect(() => {
+    fetch(
+      "https://xpert-academy-1579184148150-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
+
+ 
   return (
     <Layout>
       {isToggle && <Cart />}
