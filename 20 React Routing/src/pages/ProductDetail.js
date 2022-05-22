48fe82543b45534c81d6bf3fcc242a19b7b55076
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-22 06:57:17 +0200

    params

--- 20 React Routing/src/pages/ProductDetail.js
+++ 20 React Routing/src/pages/ProductDetail.js
@@ -1,6 +1,15 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+    const params = useParams();
+
+    console.log(params.productID);
+
+
+
     return <section>
         <h1>Product Detail</h1>
+        <p>{params.productID}</p>
     </section>
 };
 
