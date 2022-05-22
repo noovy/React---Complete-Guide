fd294d4c3761c9a7c4107d43df3016c5cd2e2032
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-22 07:12:21 +0200

    nests

--- 20 React Routing/src/pages/Products.js
+++ 20 React Routing/src/pages/Products.js
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>Proucts page</h1>
       <ul>
-        <li>Product</li>
-        <li>Product</li>
-        <li>Product</li>
-        <li>Product</li>
-        <li>Product</li>
+        <li><Link to='/products/p1'>Product</Link></li>
+        <li><Link to='/products/p2'>Product</Link></li>
+        <li><Link to='/products/p3'>Product</Link></li>
+        <li><Link to='/products/p4'>Product</Link></li>
       </ul>
     </section>
   );
--- 20 React Routing/src/pages/Welcome.js
+++ 20 React Routing/src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-    return <h1>Welcome page</h1>
+  return (
+    <section>
+      <h1>Welcome page</h1>
+      <Route path="/welcome/new-user">
+      <h1>Welcome new user</h1>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
