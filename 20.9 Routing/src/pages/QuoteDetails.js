2816d981dbee70ffe5a48d87912e06e27415984c
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-22 17:58:38 +0200

    nests

--- 20.9 Routing/src/pages/QuoteDetails.js
+++ 20.9 Routing/src/pages/QuoteDetails.js
@@ -1,31 +1,40 @@
-import { Route, useParams } from "react-router-dom";
+import { Fragment } from "react";
+import { Link, Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Max', text: 'Its fun'},
-  { id: 'q2', author: 'Max', text: 'Its fun2'},
-  { id: 'q3', author: 'Max', text: 'Its fun3'}
-]
+  { id: "q1", author: "Max", text: "Its fun" },
+  { id: "q2", author: "Max", text: "Its fun2" },
+  { id: "q3", author: "Max", text: "Its fun3" },
+];
 
 const QuoteDetails = () => {
   const params = useParams();
 
   console.log(params);
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteID);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteID);
 
   if (!quote) {
-    return (<p>No quote found.</p>)
+    return <p>No quote found.</p>;
   }
 
   return (
-    <section>
+    <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteID}/comments`}>
+      <Route path={`/quotes/${params.quoteID}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteID}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+        </Route>
+        <Route path={`/quotes/${params.quoteID}/comments`}>
           <Comments />
-      </Route>
-    </section>
+
+        </Route>
+    </Fragment>
   );
 };
 
