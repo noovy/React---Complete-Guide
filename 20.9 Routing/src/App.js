f351634f082b9f1f4b8a4ac6f5bac68859b25546
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-22 08:37:15 +0200

    wrapper

--- 20.9 Routing/src/App.js
+++ 20.9 Routing/src/App.js
@@ -3,23 +3,26 @@ import NewQuote from "./pages/NewQuote";
 import QuoteDetails from "./pages/QuoteDetails";
 
 import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes/" />
-      </Route>
-      <Route path="/quotes/" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/add-new" exact>
-        <NewQuote />
-      </Route>
-      <Route path="/quotes/:quoteID/">
-        <QuoteDetails />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes/" />
+        </Route>
+        <Route path="/quotes/" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/add-new" exact>
+          <NewQuote />
+        </Route>
+        <Route path="/quotes/:quoteID/">
+          <QuoteDetails />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
