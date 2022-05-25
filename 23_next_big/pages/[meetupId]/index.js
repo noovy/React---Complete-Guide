e4f6169a5b1bf4af490fc23584855384ec962553
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-25 16:23:22 +0200

    details

---
+++ 23_next_big/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetails";
+
+const MeetupDetails = (props) => {
+  return (
+    <MeetupDetail
+      image="https://www.werandaweekend.pl/data/articles/krakow-rynek.jpg"
+      title="Fist0"
+      address="blablabla"
+      description="ohohoh"
+    />
+  );
+};
+
+export default MeetupDetails;
